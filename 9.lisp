87dd6c4ba58cdec38d17d24e4728e69d5cf6ad59
Mikel4u4u <43415538+Mikel4u4u@users.noreply.github.com>
2019-03-18 16:15:06 +0200

    Add files via upload

---
+++ 9.lisp
@@ -0,0 +1,22 @@
+9.Определите функцию, разделяющую исходный список на два подсписка. 
+В первый из них должны попасть элементы с нечетными номерами , а во второй - 
+элементы с четными номерами .
+(defun task (lst &optional (p nil) (n nil))
+     (( lambda ( x y  )    
+    (cond       
+        ((null (cdr lst))  ( list x  n  ))
+        ((null (cddr lst)) ( list x  y  )) 
+    ( t 
+    ( task (cddr lst) x  y ))))
+               
+    (cons ( car lst )  p )  (cons (cadr lst) n)  ))
+	
+ Test case :
+
+;>( print (task '(1 2    )) )
+;>( print (task '(1 2 3 4 5 6   )) )
+;>( print (task '(1 2 3 4 5  ( 6 7 )   )) )
+
+;((1) (2))  
+;((5 3 1) (6 4 2)) 
+;((5 3 1) ((6 7) 4 2)) 
