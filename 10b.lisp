835412aee86c894d8fc5b13f3eb52e37562a36fc
Mikel4u4u <43415538+Mikel4u4u@users.noreply.github.com>
2019-03-19 17:01:20 +0200

    Add files via upload

---
+++ 10b.lisp
@@ -0,0 +1,14 @@
+10. ќпределите функцию, осуществл€ющую удаление указанного количества последних элементов исходного списка.
+(defun len(lst)(cond((null lst) 0)((+ 1 (len (cdr lst))))))(defun len-num(lst num)( set 'q    ( - ( len lst) num    ) )  (my-butlast lst q )    )(defun my-butlast(lst q)(cond(( <= q 0 ) NIL                )((< (len lst) q) NIL)((= (len lst) q) NIL)((> (len lst) q) (cons  (car lst)   ( my-butlast (cdr lst) (- q  1 )) )   )))
+
+ Test case :
+
+;>( print  (len-num '(1 2 3 4 5) 2 ) )
+;>( print  (len-num '(1 2 3 4 5 65 ( q  4) ) 2 ) )
+;>( print  (len-num '(1 ) 2 ) )
+
+
+;(1 2 3) 
+;(1 2 3 4 5) 
+;NIL 
+ 
