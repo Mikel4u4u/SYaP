5a3e56df84169ffe475cef50b3697f0ed5ae48e4
Mikel4u4u <43415538+Mikel4u4u@users.noreply.github.com>
2019-03-21 14:41:58 +0300

    Update 10b.lisp

--- 10b.lisp
+++ 10b.lisp
@@ -1,24 +1,22 @@
 10. Определите функцию, осуществл¤ющую удаление указанного количества последних элементов исходного списка.
 
 (defun len(lst)
-(cond
-((null lst) 0)
-((+ 1 (len (cdr lst))))
+      (cond
+            ((null lst) 0)
+            ((+ 1 (len (cdr lst))))
 ))
 
 (defun len-num(lst num)
-( set 'q    ( - ( len lst) num))     
-(my-butlast lst q ( len lst)  )  
-
+      (set 'q  ( - ( len lst) num))     
+      (my-butlast lst q ( len lst))  
 )
 
-(defun my-butlast(lst q  x  )
-      
-(cond
-(( <= q 0 ) NIL )
-((< x q) NIL)
-((= x q) NIL)
-((> x q) (cons  (car lst)   ( my-butlast (cdr lst) (- q  1 ) ( - x 1 ))))
+(defun my-butlast(lst q  x  )  
+      (cond
+            ((<= q 0 ) NIL)
+            ((< x q) NIL)
+            ((= x q) NIL)
+            ((> x q) (cons  (car lst)   ( my-butlast (cdr lst) (- q  1 ) ( - x 1 ))))
 ) )
 
  Test case :
