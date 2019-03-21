db5ca475fd2aa39a76fca050701ce11dbcc043b5
Mikel4u4u <43415538+Mikel4u4u@users.noreply.github.com>
2019-03-21 12:12:39 +0400

    Update 3.lisp

--- 3.lisp
+++ 3.lisp
@@ -1,19 +1,19 @@
 3. Определите функцию, заменяющую в исходном списке все вхождения заданного значения другим.
 
-(defun rep (lst w r)
+(defun rep (&key lst  w  r)
     (( lambda ( x y )      
              (cond ((null lst) nil)
-                (( eq x w) (cons r (rep y w r)))
-                ((listp x) (cons (rep x w r) (rep y w r)))
-                (t (cons x (rep y w r))))   )  
+                (( eq x w) (cons r (rep :lst y  :w w  :r r)))
+                ((listp x) (cons (rep :lst x :w w  :r r) (rep :lst y :w w  :r r)))
+                (t (cons x (rep :lst y :w w  :r r)))))  
         
-    (car lst)  ( cdr lst ) ) )  
+    (car lst) ( cdr lst ))) 
  
  Test case :
 
-;>( print (rep '(1 2 3 1 1) 1 'a) )
-;>( print (rep '((1 2 3) 4 5 6 1) 1 44) )
-;>( print (rep '( 1 2 3  ((1 2 3) (1 2 3)) )  1 '( 4 5 ) ) )
+;>( print (rep :lst '(1 2 3 1 1) :w 1 :r 'a) )
+;>( print (rep :lst '((1 2 3) 4 5 6 1) :w 1 :r 44) )
+;>( print (rep :lst '( 1 2 3  ((1 2 3) (1 2 3)) )  :w 1 :r '( 4 5 ) ) )
 
 
 
