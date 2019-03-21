136216e4e5832bc6d85a962e0d285e2741844acb
Mikel4u4u <43415538+Mikel4u4u@users.noreply.github.com>
2019-03-21 14:45:44 +0300

    Update 9.lisp

--- 9.lisp
+++ 9.lisp
@@ -1,16 +1,15 @@
-9. Определите фенкцию , разделяющую исходный список на два подсписка . В первый из них  должны попасть элементы с нечетными номерами , во 
-второй - элементы с четными номерами .
+;9. Определите фенкцию , разделяющую исходный список на два подсписка . В первый из них  должны попасть элементы с нечетными номерами , во 
+;второй - элементы с четными номерами .
 (defun task (lst &optional (p nil) (n nil))
-     (( lambda ( x y  )    
-    (cond       
-        ((null (cdr lst))  ( list x  n  ))
-        ((null (cddr lst)) ( list x  y  )) 
-    ( t 
-    ( task (cddr lst) x  y ))))
-               
-    (cons ( car lst )  p )  (cons (cadr lst) n)  ))
+	((lambda ( x y  )    
+		(cond       
+			((null (cdr lst))  ( list x  n  ))
+			((null (cddr lst)) ( list x  y  )) 
+			( t 
+			( task (cddr lst) x  y ))))          
+			(cons ( car lst )  p )  (cons (cadr lst) n)  ))
 	
- Test case :
+; Test case :
 
 ;>( print (task '(1 2    )) )
 ;>( print (task '(1 2 3 4 5 6   )) )
