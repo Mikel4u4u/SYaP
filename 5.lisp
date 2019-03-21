7055b9c51ce84793133c1873d3856bf5585e18e3
Mikel4u4u <43415538+Mikel4u4u@users.noreply.github.com>
2019-03-21 14:44:39 +0300

    Update 5.lisp

--- 5.lisp
+++ 5.lisp
@@ -1,15 +1,15 @@
-5.Определите функцию , которая увеличивет элеметы исходного списка на еденицу .
+;5.Определите функцию , которая увеличивет элеметы исходного списка на еденицу .
 
 (defun plus1 ( lst )
-     (( lambda ( x y ) 
+          ((lambda ( x y ) 
                (cond ((null lst) nil)
                     ((atom x)   (cons  ( + x 1 )( plus1 y)))
                     ((listp x)  (cons  ( plus1 x )( plus1 y))))            
-     (car lst)
-     (cdr lst)      
-         ))
+          (car lst)
+          (cdr lst)      
+ ))
  
- Test case :
+ ;Test case :
 
 ;>( print  (plus1 '( 1 2 3 4 0 ) ) )
 ;>( print  (plus1 '( ( 1 2 ) 3 4 0 ) ) )
