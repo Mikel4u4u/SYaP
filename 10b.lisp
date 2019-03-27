fc7e788620f9d2cb1b39a6395271d41aab801036
Mikel4u4u <43415538+Mikel4u4u@users.noreply.github.com>
2019-03-27 16:39:23 +0300

    Update 10b.lisp

--- 10b.lisp
+++ 10b.lisp
@@ -1,29 +1,28 @@
 ;10. Определите функцию, осуществл¤ющую удаление указанного количества последних элементов исходного списка.
 
 (defun len(lst)
-      (cond
-            ((null lst) 0)
-            ((+ 1 (len (cdr lst))))
-))
+    (cond
+        ((null lst) 0)
+        ((+ 1 (len (cdr lst))))
+    ))
 
-(defun len-num(lst num)
-      (set 'q  ( - ( len lst) num))     
-      (my-butlast lst q ( len lst))  
+(defun F (L N C)
+    (if L
+        (if (> C 0)
+            (cons (car L) (F (cdr L) N (1- C))))
+    )
 )
 
-(defun my-butlast(lst q  x  )  
-      (cond
-            ((<= q 0 ) NIL)
-            ((< x q) NIL)
-            ((= x q) NIL)
-            ((> x q) (cons  (car lst)   ( my-butlast (cdr lst) (- q  1 ) ( - x 1 ))))
-) )
+(defun dellast (L N)
+ (F L N (- (len L) N)))
+
+
 
 ; Test case :
 
-;>( print  (len-num '(1 2 3 4 5) 2 ) )
-;>( print  (len-num '(1 2 3 4 5 65 ( q  4) ) 2 ) )
-;>( print  (len-num '(1 ) 2 ) )
+;>( print  (dellast '(1 2 3 4 5) 2 ) )
+;>( print  (dellast '(1 2 3 4 5 65 ( q  4) ) 2 ) )
+;>( print  (dellast '(1 ) 2 ) )
 
 
 ;(1 2 3) 
