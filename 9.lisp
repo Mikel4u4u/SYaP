c43e4164dcbaed7f1995444f328df5272464c93c
Mikel4u4u <43415538+Mikel4u4u@users.noreply.github.com>
2019-03-28 09:28:07 +0200

    Update 9.lisp

--- 9.lisp
+++ 9.lisp
@@ -1,11 +1,11 @@
 ;9. Определите фенкцию , разделяющую исходный список на два подсписка . В первый из них  должны попасть элементы с нечетными номерами , во 
 ;второй - элементы с четными номерами .
-(defun F ( L )
-    (if L
+(defun chetnechet ( lst )
+    (if lst
         (list
-            (cons (car L)  (car (F (cddr L))))
-            (if (cadr L)
-                (cons (cadr L)   (cadr (F (cddr L))))
+            (cons (car lst)  (car (chetnechet (cddr lst))))
+            (if (cadr lst)
+                (cons (cadr lst)   (cadr (chetnechet (cddr lst))))
             )
         ) 
     ) 
@@ -13,9 +13,9 @@
 	
 ; Test case :
 
-;>( print (f '(1 2    )))
-;>( print (f '(1 2 3 4 5 6   )))
-;>( print (f '(1 2 3 4 5  6 7    )))
+;>( print (chetnechet '(1 2    )))
+;>( print (chetnechet '(1 2 3 4 5 6   )))
+;>( print (chetnechet '(1 2 3 4 5  6 7    )))
 
 ;((1) (2)) 
 ;((1 3 5) (2 4 6)) 
