57e3e5cab41b0f88352ebc82d233572e5f45da42
Mikel4u4u <43415538+Mikel4u4u@users.noreply.github.com>
2019-04-11 19:52:42 +0300

    Update 5Высшие.lisp

--- 5Высшие.lisp
+++ 5Высшие.lisp
@@ -1,5 +1,18 @@
+;5. Определите функциональный предикат (НЕКОТОРЫй пред список), который истинен,когда,
+;являющейся функциональным аргументом предикат пред истинен хотя бы для одного элемента списка список.
 
-(defun mnogofun (w x)    
-  (mapcar #' (lambda (f) (funcall f x)) w))
+(defun mypred (func  args)
+             ( mysearch   (mapcar func args)))
 
-(print (mnogofun '(atom zerop) '0 ))  
+(defun mysearch (lst)
+    (cond
+       ((eq (car lst) T)  T)
+       ((eq (cdr lst) nil)  nil)
+       (t (mysearch (cdr lst )))
+    )   
+)
+
+
+(print (mypred #' atom '(1 2 3 4) )) ;=> T
+(print (mypred #' zerop '(1 2 3 4) )) ;=> NIL 
+(print (mypred #' zerop '(1 2 4 0) )) ;=> T 
