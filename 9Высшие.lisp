ee8f1c4cf392302cf6e3a6830d8ae07b38ef8947
Mikel4u4u <43415538+Mikel4u4u@users.noreply.github.com>
2019-04-24 21:24:05 +0300

    Update 9Высшие.lisp

--- 9Высшие.lisp
+++ 9Высшие.lisp
@@ -1,65 +1,28 @@
 ;9. Напишите генератор порождения чисел Фибоначчи: 0, 1, 1, 2, 3, 5, ...
 (let ((curr 1) (prev 0))
-  (defun next-fib  nil (let ((a curr)) (setq curr (+ curr prev) prev a)))
-  (defun reset-fib nil (setq curr 1 prev 0))) 
+  (defun next-fib  nil 
+     
+      (let 
+       ((a curr))
+       (setq prev1 prev)
+       (setq curr (+ curr prev) prev a) 
+       (setq curr1 prev1)
+       )
+  )
+) 
  
-(next-fib)
- 
-==> 1
-(next-fib)
- 
-==> 1
-(next-fib)
- 
-==> 2
-(next-fib)
- 
-==> 3
-(next-fib)
- 
-==> 5
-(next-fib)
- 
-==> 8
-(next-fib)
- 
-==> 13
+(print (next-fib))
+(print (next-fib))
+(print (next-fib))
+(print (next-fib))
+(print (next-fib))
+(print (next-fib))
+(print (next-fib))
 
-(defun generator (&optional(a 0))
-    (let
-         ((numb a))
-     (lambda () (setq numb (+ numb 1)))
-     )
-    )
-(defun task1 ()
-   (if (> (random 10) 5 ) (print (list (funcall next-number1))) (print (funcall next-number2)) )
-
-
-)
-(setq  next-number1 (generator))
-(setq next-number2 (generator))
-
-( task1)
-( task1)
-
-
-
-(defun generator (&optional(a 0))
-    (let
-         ((numb 0))
-     (lambda () (setq numb (+ (generator (- numb 1   ) )    (generator  (- numb 2 )  )  ) ))
-     )
-    )
-
-(defun task1 ()
-  (print (funcall next-number2)) 
-
-
-)
-
-(setq next-number2 (generator))
-
-( task1)
-( task1)
-( task1)
-( task1)
+;0 
+;1 
+;1 
+;2 
+;3 
+;5 
+;8 
