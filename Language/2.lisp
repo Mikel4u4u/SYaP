0266d585709da50859d68e1d52db426ffba053d2
Mikel4u4u <43415538+Mikel4u4u@users.noreply.github.com>
2019-05-16 02:11:19 +0300

    Update 2.lisp

--- Language/2.lisp
+++ Language/2.lisp
@@ -1,17 +1,17 @@
 (defun хим_реакция (lst1 lst2 )
     
     (list 
-      lst1 '+ lst2
-      '->
-      (list  (car lst1) (cadr lst2)  (caddr lst2 ) (cadddr lst1 ))
-      '+
-      (list  (car lst2) (cadr lst1)  (caddr lst1 ) (cadddr lst2 ))
+		 lst1 '+ lst2
+	    '->
+		(list  (car lst1) (cadr lst2)  (caddr lst2 ) (cadddr lst1 ))
+          '+
+        (list  (car lst2) (cadr lst1)  (caddr lst1 ) (cadddr lst2 ))
     )
     
 )   
 
 (defun определить (lst1 )
-
+    
 	(cond 
 		((eq (caddr lst1) 'OH )  'csheloch )
 		((eq (car lst1) 'H )  'kislota )
@@ -19,7 +19,13 @@
 	)
 )   
 
- 
+
+(defun что_на_входе_и_выходе (lst1 )
+(mapcan #'  (lambda (x)   (if (atom x) 
+				 (list x) 
+				    (list (funcall 'определить x))))  
+	     lst1 )
+)   
 
 
 (defmacro Химия (&rest args)
@@ -33,7 +39,9 @@
 		(cond
 		
 			((eq operation 'хим_реакция) (setq val (хим_реакция (car реагенты )(caddr реагенты ))))			
-			((eq operation 'определить) (setq val (определить  (car реагенты )  )))			                    			
+			((eq operation 'определить) (setq val (определить  (car реагенты ))))
+			((eq operation 'что_на_входе_и_выходе ) (setq val (что_на_входе_и_выходе  (car реагенты ))))
+			
 		)
 		(print val)		
 	)    
