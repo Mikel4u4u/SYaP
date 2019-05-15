73fca9220505a86d36bd9cd89f76c798539364d1
Mikel4u4u <43415538+Mikel4u4u@users.noreply.github.com>
2019-05-15 18:13:38 +0300

    Update 4.lisp

--- Макросы/4.lisp
+++ Макросы/4.lisp
@@ -1,9 +1,8 @@
 ;Определите в виде макроса форму (FIF тест отр нуль полож).
 (defmacro FIF (test neg zer pos)
-  `(let ((ttest ,test) (nneg ,neg) (zzer ,zer) (ppos ,pos))
-      (cond ((zerop ttest) zzer)
-            ((minusp ttest) nneg)
-            (t ppos))))
+  `(cond ((zerop ,ttest) ,zzer)
+            ((minusp ,ttest) ,nneg)
+            (t ,ppos)))
  
  
 
